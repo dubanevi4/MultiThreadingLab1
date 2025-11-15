49329441a88a482f49c90ad4191b8a037acf6fc2
dubanevi4 <aliaksei.dubanevich@student.ehu.lt>
2025-11-15 10:39:36 +0300

    Task1

---
+++ src/main/java/lt/esdc/multithreading/lab1/task1/MainThreadTask1.java
@@ -0,0 +1,15 @@
+package lt.esdc.multithreading.lab1.task1;
+
+
+public class MainThreadTask1 {
+    public static int COUNTER = 10;
+
+    public static void main(String[] args){
+
+        Thread walkthread = new WalkThreadTask1();
+        walkthread.start();
+        for (int i = 0; i < COUNTER; i++){
+            System.out.println(Thread.currentThread().getName() + " run " + i );
+        }
+    }
+}
---
+++ src/main/java/lt/esdc/multithreading/lab1/task1/WalkThreadTask1.java
@@ -0,0 +1,11 @@
+package lt.esdc.multithreading.lab1.task1;
+
+public class WalkThreadTask1 extends Thread{
+
+    @Override
+    public void run(){
+        for (int i = 0; i < MainThreadTask1.COUNTER; i++){
+            System.out.println(Thread.currentThread().getName() + " run " + i );
+        }
+    }
+}
